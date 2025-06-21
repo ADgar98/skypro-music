742a4fdfaa890ce0d92305a9be2b9826b85acade
Adgar <YOUR EMAIL@SAMLECOM>
2025-06-21 15:00:22 +0300

    исправил замечания

--- src/app/components/Filter/Filter.tsx
+++ src/app/components/Filter/Filter.tsx
@@ -2,8 +2,6 @@
 import { useState } from 'react';
 import MusicianModalWin from '../MusicianModalWin/MusicianModalWin';
 import styles from './filter.module.css';
-import { getUniqueValuesByKey } from '@/utils/helpers';
-import { data } from '@/data';
 import DateFilterModWin from '../DateFilterModWin/DateFilterModWin';
 import GenreFilterModWin from '../GenreFilterModWin/GenreFilterModWin';
 
@@ -29,47 +27,46 @@ export default function Filter() {
     setMusicianWin(false);
   };
 
-  const ArrOfAuthor = getUniqueValuesByKey(data, 'author');
-
-  const numberOfMusician = ArrOfAuthor.length;
-
-  const ArrOfGenre = getUniqueValuesByKey(data, 'genre');
-
-  const numberOfGenre = ArrOfGenre.length
-
   return (
     <div className={styles.centerblock__filter}>
       <div className={styles.filter__title}>Искать по:</div>
-      <div
-        onClick={openMusicianWin}
-        data-active={musicianWin}
-        className={`${styles.filter__button} ${musicianWin ? styles.active : ''}`}
-      >
-        исполнителю
-        {musicianWin && (
-          <span className={styles.filter__index}>{numberOfMusician}</span>
-        )}
+      <div className={styles.filter__container}>
+        <div
+          onClick={openMusicianWin}
+          data-active={musicianWin}
+          className={`${styles.filter__button} ${musicianWin ? styles.active : ''}`}
+        >
+          исполнителю
+        </div>
+        <div className={styles.filterbox}>
+          {musicianWin && <MusicianModalWin />}
+        </div>
       </div>
-      {musicianWin && <MusicianModalWin />}
-      <div
-        onClick={openDateWin}
-        data-active={dateWin}
-        className={`${styles.filter__button} ${dateWin ? styles.active : ''}`}
-      >
-        году выпуска
+
+      <div className={styles.filter__container}>
+        <div
+          onClick={openDateWin}
+          data-active={dateWin}
+          className={`${styles.filter__button} ${dateWin ? styles.active : ''}`}
+        >
+          году выпуска
+        </div>
+        <div className={styles.filterbox}>
+          {dateWin && <DateFilterModWin />}
+        </div>
       </div>
-      {dateWin && <DateFilterModWin />}
-      <div
-        onClick={openGenreWin}
-        data-active={genreWin}
-        className={`${styles.filter__button} ${genreWin ? styles.active : ''}`}
-      >
-        жанру
-        {genreWin && (
-          <span className={styles.filterGenre__index}>{numberOfGenre}</span>
-        )}
+      <div className={styles.filter__container}>
+        <div
+          onClick={openGenreWin}
+          data-active={genreWin}
+          className={`${styles.filter__button} ${genreWin ? styles.active : ''}`}
+        >
+          жанру
+        </div>
+        <div className={styles.filterbox}>
+          {genreWin && <GenreFilterModWin />}
+        </div>
       </div>
-      {genreWin && <GenreFilterModWin />}
     </div>
   );
 }
