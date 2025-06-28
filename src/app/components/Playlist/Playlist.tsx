0c87aee4266cab0d5b36272ab78e924b3819ecc6
Adgar <YOUR EMAIL@SAMLECOM>
2025-06-28 21:19:25 +0300

    добавил воспроизведение музыки и Redux

--- src/app/components/Playlist/Playlist.tsx
+++ src/app/components/Playlist/Playlist.tsx
@@ -1,7 +1,28 @@
+'use client'
 import styles from './playlist.module.css';
 import PlaylistItem from '../PlaylistItem/PlaylistItem';
+import { fetchMusic } from '@/api';
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/store/store';
+import { setAllTracks } from '@/store/features/trackSlice';
 
 export default function Playlist() {
+  const dispatch = useAppDispatch()
+  
+  useEffect(() => {
+  const loadTracks = async () => {
+    try {
+      const tracks = await fetchMusic(); 
+      dispatch(setAllTracks(tracks));     
+    } catch (error) {
+      console.error('Не удалось загрузить треки', error);
+    }
+  };
+
+  loadTracks();
+  
+  
+}, [dispatch]);
   return (
     <div className={styles.content__playlist}>
       <PlaylistItem/>
